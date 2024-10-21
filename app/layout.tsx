f959dfedd2aa4887261146e4e474243a13b739c2
Felipi Lima Matozinho <matozinhof.academico@gmail.com>
2024-10-21 14:34:15 -0300

    feat(layout.tsx): enhance layout with improved theme selection UI for better user experience feat(page.tsx): update home page structure to better showcase theme examples and improve layout fix(tailwind.config.ts): adjust background colors for better theme representation and add new color utilities
    Signed-off-by: Felipi Lima Matozinho <matozinhof.academico@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,44 +7,54 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { cn } from "@/lib/utils";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
 });
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  const [theme, setTheme] = useState("");
+	const [theme, setTheme] = useState("");
 
-  const handleTheme = (theme: string) => {
-    setTheme(theme === "default" ? "" : theme);
-  };
+	const handleTheme = (theme: string) => {
+		setTheme(theme === "default" ? "" : theme);
+	};
 
-  return (
-    <html lang="en">
-      <body
-        className={cn(
-          `${geistSans.variable} ${geistMono.variable} dark`,
-          theme
-        )}
-      >
-        <RadioGroup onValueChange={handleTheme} defaultValue="default">
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="default" id="r1" />
-            <label htmlFor="r1">Default</label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="example_1" id="r2" />
-            <label htmlFor="r2">example_1</label>
-          </div>
-        </RadioGroup>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={cn(
+					`${geistSans.variable} ${geistMono.variable} dark`,
+					theme,
+				)}
+			>
+				<div className="w-full flex justify-center">
+					<RadioGroup
+						className="flex p-4 my-2 mb-8 rounded-lg bg-secondary"
+						onValueChange={handleTheme}
+						defaultValue="default"
+					>
+						<div className="flex items-center space-x-2">
+							<RadioGroupItem value="default" id="r1" />
+							<label className="text-lg font-medium" htmlFor="r1">
+								Default
+							</label>
+						</div>
+						<div className="flex items-center space-x-2">
+							<RadioGroupItem value="example_1" id="r2" />
+							<label className="text-lg font-medium" htmlFor="r2">
+								example_1
+							</label>
+						</div>
+					</RadioGroup>
+				</div>
+				{children}
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,34 @@
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="bg-tailwind-with-theme p-2 rounded">
-        <p className="text-example">
-          without tailwindcss-themer: theme example
-        </p>
-      </div>
-      <div className="bg-tailwind-with-opacity p-2 rounded">
-        <p className="text-example">
-          without tailwindcss-themer: opacity example
-        </p>
-      </div>
-      <div className="bg-with-theme p-2 rounded">
-        <p className="text-example">with tailwindcss-themer: opacity example</p>
-      </div>
-      <div className="bg-with-opacity p-2 rounded">
-        <p className="text-example">
-          with tailwindcss-themer: Color alias example
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="font-sans flex flex-col items-center justify-items-center min-h-screen gap-16">
+			<section className="flex flex-col gap-2">
+				<h2 className="text-2xl font-semibold">Without Tailwindcss-themer</h2>
+				<div className="flex gap-2">
+					<div className="bg-tailwind-with-theme p-2 rounded">
+						<p className="text-example">custom color example</p>
+					</div>
+					<div className="bg-tailwind-with-opacity p-2 rounded">
+						<p className="text-example">opacity example</p>
+					</div>
+          <div className="bg-tailwind-normal-color p-2 rounded">
+						<p className="text-example">normal color example</p>
+					</div>
+				</div>
+			</section>
+			<section className="flex flex-col gap-2">
+				<h2 className="text-2xl font-semibold">With Tailwindcss-themer</h2>
+				<div className="flex gap-2">
+					<div className="bg-with-theme p-2 rounded">
+						<p>Theme func example</p>
+					</div>
+					<div className="bg-with-opacity p-2 rounded">
+						<p>Opacity example</p>
+					</div>
+          <div className="bg-working p-2 rounded">
+						<p>Working example</p>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 }
